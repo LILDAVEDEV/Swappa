a8e52e9a16d9256492f4c5e309a03574e668364b
LILDAVEDEV <timilehinobawole18@gmail.com>
2024-01-07 00:04:29 +0100

    first commit

---
+++ javascript/bitcoin.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { firestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBbXAAeuDeAjfdkfuixscAC-ey7-_uJ96o",
+  authDomain: "bitcoin-61ca9.firebaseapp.com",
+  projectId: "bitcoin-61ca9",
+  storageBucket: "bitcoin-61ca9.appspot.com",
+  messagingSenderId: "886069169533",
+  appId: "1:886069169533:web:5c7a175d3d8fc01dfcd014"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
